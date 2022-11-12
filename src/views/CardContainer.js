78fb4c9a73c05bf8901fa2f326d8cb0a409448d0
Krista M <morales-k@outlook.com>
2022-11-11 19:26:50 -0500

    Restructured CardContainer
    Destructured props. Updated shuffle function. Card creation now based on matches.

--- src/views/CardContainer.js
+++ src/views/CardContainer.js
@@ -5,6 +5,9 @@ import * as Coffee from '../assets/Coffee';
 import matchSound from '../assets/sounds/match.mp3';
 
 function CardContainer(props) {
+  const { resetGame, handleFlipCount, handleScore, numberOfMatches } = props;
+  const [coffeeMatches, setCoffeeMatches] = useState([]);
+  const [donutMatches, setDonutMatches] = useState([]);
   const [cards, setCards] = useState([]);
   const [prevCard, setPrevCard] = useState({});
   const [currentCard, setCurrentCard] = useState({});
@@ -12,10 +15,16 @@ function CardContainer(props) {
   let [flipCount, setFlipCount] = useState(0);
 
   useEffect(() => {
-    if (cards.length === 0 || props.resetGame) {
+    if (cards.length === 0 || resetGame) {
+      setMatches();
+    }
+  }, [cards.length, resetGame]);
+
+  useEffect(() => {
+    if (!!coffeeMatches?.length && !!donutMatches?.length) {
       createCards();
     }
-  }, [cards.length, props.resetGame]);
+  }, [coffeeMatches, donutMatches])
 
   useEffect(() => {
     if (prevCard === null) {
@@ -27,7 +36,7 @@ function CardContainer(props) {
 // Flips cards back after 2 cards are flipped if no match is found.
 function handleCards(currentTarget, clickedCard) {
   setFlipCount(flipCount += 1);
-  props.handleFlipCount();
+  handleFlipCount();
 
   clickedCard.flipped = true;
 
@@ -40,7 +49,7 @@ function handleCards(currentTarget, clickedCard) {
       prevCard.matchFound = true;
       currentTarget.classList.add('flip');
       setFlipCount(0); 
-      props.handleScore();
+      handleScore();
     } else if (clickedCard.matchIndex !== prevCard.matchIndex || clickedCard.type !== prevCard.type || clickedCard.id === prevCard.id) {
         // Flip previous card back if not part of a found match.
         setTimeout(() => {
@@ -61,57 +70,89 @@ function handleCards(currentTarget, clickedCard) {
   setPrevTarget(currentTarget);
 }
 
-// Finds a match for the current card loop & index.
-function selectMatch(loop, i) {
-  let currentMatch = '';
-  let object = loop === 0 ? Donuts : Coffee;
+/**
+ * Creates & shuffles an array of key names from the Coffee and Donut objects.
+ * Then takes the array & matches the key names to the objects index, 
+ * creating an array of random index matches.
+ * 
+ */
+function setMatches() {
+  let count = 0;
 
-    Object.keys(object).map((obj, index) => {
-      if (i === 24) {
-        currentMatch = 0;
-      } else if (i - 1 === index || i - 12 === index) {
-        currentMatch = index;
-      }
-    });
+  while (count < 2) {
+    let deckImages = count === 0 ? Donuts : Coffee;
+    let deckKeys = Object.keys(deckImages);
+    let shuffledImages = shuffleArray(deckKeys).splice(0, (numberOfMatches / 2));
+    let donutIndexes = [];
+    let coffeeIndexes = [];
 
-  if (currentMatch !== '') {
-    return currentMatch;
+    Object.keys(deckImages).forEach((key, index) => {
+      shuffledImages.forEach(image => {
+        if (key === image && count === 0) {
+          donutIndexes.push(index)
+        } else if (key === image && count === 1) {
+          coffeeIndexes.push(index)
+        }
+      })
+    })
+
+    if (count === 0) {
+      setDonutMatches(donutIndexes)
+    } else {
+      setCoffeeMatches(coffeeIndexes)
+    }
+
+    count++;
   }
 }
 
-// Creates an array of card objects.
+/**
+ * Uses Fischer-Yates shuffle to randomize array items.
+ * 
+ * @param {Array} arr - Array of items to shuffle.
+ * @returns 
+ */
+function shuffleArray(arr) {
+  const newArr = [];
+
+  while (arr.length) {
+    const randomIndex = Math.floor(Math.random() * arr.length),
+        element = arr.splice(randomIndex, 1);
+
+    newArr.push(element[0]);
+    }
+    return newArr
+}
+
+// Creates an array of cards for each card type.
 function createCards() {
   const allCards = [];
   let loop = 0;
+  let currentDeckMatch = 0; // A number indicating which index of the current deck to use for matching. (coffee/donut matches)
+  let numberOfDeckMatches = numberOfMatches / 2; // Always half of the total# of matches.
 
   while (loop < 2) {
-    for (let i = 1; i < props.numberOfMatches + 1; i++) {
-      // matchIndex: Index of a type's component array.
+    for (let i = 1; i < numberOfMatches + 1; i++) {
       let card = {
-        id: loop === 0 ? i : props.numberOfMatches + i,
+        id: loop === 0 ? i : numberOfMatches + i,
         type: loop === 0 ? 'Donut' : 'Coffee',
-        matchIndex: selectMatch(loop, i),
+        matchIndex: loop === 0 ? donutMatches[currentDeckMatch] : coffeeMatches[currentDeckMatch],
         matchFound: false,
         flipped: false,
       }
       allCards.push(card);
+
+      // Reset currentDeckMatch to avoid adding an invalid match index.
+      if (currentDeckMatch + 1 === numberOfDeckMatches) {
+        currentDeckMatch = 0;
+      } else {
+        currentDeckMatch++;
+      }
+      
     }
     loop++;
   }
-  shuffleCards(allCards);
-}
-
-// Uses Fischer-Yates shuffle to randomize cards.
-function shuffleCards(allCards) {
-  const newArr = [];
-
-  while (allCards.length) {
-    const randomIndex = Math.floor(Math.random() * allCards.length),
-        element = allCards.splice(randomIndex, 1);
-
-    newArr.push(element[0]);
-    }
-    setCards(newArr);
+  setCards(shuffleArray(allCards));
 }
 
   return (
